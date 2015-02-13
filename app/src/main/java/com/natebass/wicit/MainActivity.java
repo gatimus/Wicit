a75956ff514c997bb7175c4de98db29d0406e16b
Nate Bass <doublenatebass@gmail.com>
2015-02-12 19:25:29 -0800

    Added JSON libraries

--- app/src/main/java/com/natebass/wicit/MainActivity.java
+++ app/src/main/java/com/natebass/wicit/MainActivity.java
@@ -1,6 +1,7 @@
 package com.natebass.wicit;
 
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -16,12 +17,21 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 public class MainActivity extends ActionBarActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks {
-    public static final String BASE_URL = "http://cdph.data.ca.gov/resource/i7wi-ei4m.json";
-    public static final String SOCRATA_APP_TOKEN = "1DpALfWSiKZLlyC4IPBnGszwN";
-    
     private GoogleApiClient mGoogleApiClient;
     private GoogleMap mMap;
+    private JSONObject jsonResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,7 +89,7 @@ public class MainActivity extends ActionBarActivity implements OnMapReadyCallbac
             @Override
             public void onCameraChange(CameraPosition position) {
                 LatLngBounds bounds = mMap.getProjection().getVisibleRegion().latLngBounds;
-                fetchData(bounds);
+                new FetchData().execute(bounds);
             }
         });
         
@@ -88,13 +98,6 @@ public class MainActivity extends ActionBarActivity implements OnMapReadyCallbac
                 .title("Sydney")
                 .snippet("The most populous city in Australia.")
                 .position(defaultLocation));*/
-    }
-
-    private void fetchData(LatLngBounds bounds) {
-        String locationsUrl = BASE_URL + "?$where=within_box(location," +
-                bounds.southwest.longitude + "," + bounds.southwest.latitude + "," +
-                bounds.northeast.latitude + "," + bounds.northeast.longitude + ")";
-        locationsUrl += "&$$app_token=" + SOCRATA_APP_TOKEN;
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -114,5 +117,52 @@ public class MainActivity extends ActionBarActivity implements OnMapReadyCallbac
 
     @Override
     public void onConnectionSuspended(int i) {
+    }
+    
+    class FetchData extends AsyncTask<LatLngBounds, Void, Boolean> {
+        // todo: Limit the number of requests, maybe cache something
+        public static final String BASE_URL = "http://cdph.data.ca.gov/resource/i7wi-ei4m.json";
+        public static final String SOCRATA_APP_TOKEN = "1DpALfWSiKZLlyC4IPBnGszwN";
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Boolean doInBackground(LatLngBounds... bounds) {
+            try {
+                String locationsUrl = "";
+                // todo: This loop is misleading because I only intend to pass in one LatLongBounds
+                for (LatLngBounds bound : bounds) {
+                    locationsUrl = BASE_URL + "?$where=within_box(location," +
+                            bound.northeast.latitude + "," + bound.northeast.longitude + "," +
+                            bound.southwest.latitude + "," + bound.southwest.longitude + ")";
+                }
+                locationsUrl += "&$$app_token=" + SOCRATA_APP_TOKEN;
+                HttpGet httpGet = new HttpGet(locationsUrl);
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpResponse response = httpclient.execute(httpGet);
+
+                // StatusLine stat = response.getStatusLine();
+                int status = response.getStatusLine().getStatusCode();
+
+                if (status == 200) {
+                    HttpEntity entity = response.getEntity();
+                    String data = EntityUtils.toString(entity);
+                    jsonResult = new JSONObject(data);
+                    return true;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+        }
     }
 }
